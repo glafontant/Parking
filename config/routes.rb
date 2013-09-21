6f21fb5016cf136305274eebbcb0776f17cd14a3
Gabriel Lafontant <glafonta@gmail.com>
2013-09-21 18:26:30 -0400

    modified routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Parking::Application.routes.draw do
   resources :parking_registrations
 
-root 'parking_registrations#index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'parking_registrations#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
